d8467f1bfa88de93d4f7210c7d39814c2692adc7
Charis Logothetis <18629554+haris-git@users.noreply.github.com>
2020-07-31 14:06:44 +0300

    Remove comment from ProductWithHooks component

--- src/components/ProductWithHooks/ProductWithHooks.js
+++ src/components/ProductWithHooks/ProductWithHooks.js
@@ -34,10 +34,6 @@ function totalReducer(state, price) {
 }
 
 export default function ProductWithHooks() {
-  /**
-   * "useState" is a function that takes the initial state as an argument and returns an array with two items. 
-   * The first item is a variable containing the state, and the second item in the array is a function that will update the state
-   */
   const [cart, setCart] = useReducer(cartReducer, []);
   const [total, setTotal] = useReducer(totalReducer, 0);
 
