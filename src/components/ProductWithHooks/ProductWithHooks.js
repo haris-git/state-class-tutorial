aef36088d34d1ea6db88ddefe74b0856e9dd96f4
Charis Logothetis <18629554+haris-git@users.noreply.github.com>
2020-07-31 12:09:13 +0300

    Setting state with "useState"

--- src/components/ProductWithHooks/ProductWithHooks.js
+++ src/components/ProductWithHooks/ProductWithHooks.js
@@ -1,16 +1,44 @@
 import React, { useState } from 'react';
 import './ProductWithHooks.css';
 
+const currencyOptions = {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+}
+
 export default function ProductWithHooks() {
+  /**
+   * "useState" is a function that takes the initial state as an argument and returns an array with two items. 
+   * The first item is a variable containing the state, and the second item in the array is a function that will update the state
+   */
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  function getTotal(total ) {
+    return total.toLocaleString(undefined, currencyOptions)
+  }
+
+  function add() {
+    setCart(['ice cream']);
+    setTotal(5);
+  }
+
   return(
     <div className="wrapper">
       <div>
-        Shopping Cart: 0 total items.
+        Shopping Cart: { cart.length } total items.
       </div>
-      <div>Total: 0</div>
+      <div>Total: { getTotal(total) }</div>
 
       <div className="product-with-hooks"><span role="img" aria-label="ice cream">🍦</span></div>
-      <button>Add</button> <button>Remove</button>
+      <button onClick={ add }>Add</button> 
+      <button onClick={ 
+        // This arrow function is not performance-optimized.
+        () => { 
+          setCart([]); 
+          setTotal(0); 
+        } 
+      }>Remove</button> 
     </div>
   )
 }
